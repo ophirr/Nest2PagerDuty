a5e088367c4f3657dadd2e26cf4a7c6c836894df
Ophir Ronen <ophir@tizos.org>
2020-04-06 11:48:26 -0700

    support KCSAR

--- Nest2PagerDuty.py
+++ Nest2PagerDuty.py
@@ -4,6 +4,9 @@ import json
 import requests
 from sekret import api_key, GNAME, GP, ROUTING_KEY, INCIDENT_KEY
 
+extsub = ''
+nest_url = ''
+
 try:
     from BeautifulSoup import BeautifulSoup
 except ImportError:
@@ -12,7 +15,7 @@ except ImportError:
 # Login to gmail, yes this mechanism is insecure
 g = gmail.login(GNAME, GP)
 
-def trigger_incident():
+def trigger_nest_incident():
     # Triggers a PagerDuty incident without a previously generated incident key
     # Uses Events V2 API - documentation: https://v2.developer.pagerduty.com/docs/send-an-event-events-api-v2
 
@@ -24,16 +27,48 @@ def trigger_incident():
         "routing_key": ROUTING_KEY,
         "event_action": "trigger",
         "dedup_key": INCIDENT_KEY,
-        "class": "Security",
         "payload": {
             "summary": extsub,
             "source": "Nest Camera Infra",
-            "severity": "warning"
+            "severity": "warning",
+            "class": "security"
         },
         "links": [{
             "href": nest_url,
             "text": ">>> CLICK HERE to view the footage <<<"
-        }]
+        }],
+
+    }
+
+    response = requests.post('https://events.pagerduty.com/v2/enqueue',
+                             data=json.dumps(payload),
+                             headers=header)
+
+    if response.json()["status"] == "success":
+        print('Incident Created')
+    else:
+        print(response.text)  # print error message if not successful
+
+
+
+def trigger_sar_incident():
+    # Triggers a PagerDuty incident without a previously generated incident key
+    # Uses Events V2 API - documentation: https://v2.developer.pagerduty.com/docs/send-an-event-events-api-v2
+
+    header = {
+        "Content-Type": "application/json"
+    }
+
+    payload = {  # Payload is built with the least amount of fields required to trigger an incident
+        "routing_key": ROUTING_KEY,
+        "event_action": "trigger",
+        "dedup_key": INCIDENT_KEY,
+        "payload": {
+            "summary": extsub,
+            "source": "KCSAR",
+            "severity": "critical",
+            "class": "search and rescue"
+        }
     }
 
     response = requests.post('https://events.pagerduty.com/v2/enqueue',
@@ -54,31 +89,41 @@ else :
 #unread = g.mailbox('NestAlerts').mail(unread = 'True', prefetch = 'True')
 unread = g.mailbox('NestAlerts').mail(unread = 'True')
 
-
 for number in unread:
 
     number.fetch()
     nest_html = number.html
 
-    soup = BeautifulSoup(nest_html, features="html.parser")
+    if nest_html is not None:
+        soup = BeautifulSoup(nest_html, features="html.parser")
 
-    soup.find('a')
-    for link in soup.find_all('a'):
-        nest_url = link.get('href')
+        soup.find('a')
+        for link in soup.find_all('a'):
+                nest_url = link.get('href')
         break
 
-    spans = soup.find_all('span')
-    for span in spans:
-        extsub = span.text
+        spans = soup.find_all('span')
+        for span in spans:
+            extsub = span.text
         break
 
+    # Let's check out any attachments
+    #goodies = number.attachments
+    #image = goodies[0]
     #print "URL - '" + nest_url + "'"
     #print "SUBJECT - '" + extsub  + "'"
 
-    # Mark message as read
-    number.read()
+        trigger_nest_incident()
 
-    trigger_incident()
+    else:
+
+        extsub = number.body
+        trigger_sar_incident()
+
+ # Mark message as read
+number.read()
+
+
 
 
 if not unread:
